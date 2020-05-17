adde93aa9c01e5dea897cb1388c2ad6c1f13254b
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2020-05-17 13:18:38 -0400

    mobile responsive modification added.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Square from "./square";
+import Restart from "./Restart";
+import "./App.css";
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+  const nextSymbol = isXNext ? "X" : "O";
+  const winner = calculateWinner(squares);
+
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          if (squares[i] != null || winner != null) {
+            return;
+          }
+          const nextSquares = squares.slice();
+          nextSquares[i] = nextSymbol;
+          setSquares(nextSquares);
+
+          setIsXNext(!isXNext); // toggle turns
+        }}
+      />
+    );
+  }
+  function getStatus() {
+    if (winner) {
+      return "Winner: " + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw!";
+    } else {
+      return "Next player: " + nextSymbol;
+    }
+  }
+
+  function isBoardFull(squares) {
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i] == null) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function calculateWinner(squares) {
+    const possibleLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    // go over all possibly winning lines and check if they consist of only X's/only O's
+    for (let i = 0; i < possibleLines.length; i++) {
+      const [a, b, c] = possibleLines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function renderRestartButton() {
+    return (
+      <Restart
+        onClick={() => {
+          setSquares(Array(9).fill(null));
+          setIsXNext(true);
+        }}
+      />
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="game">
+        <div className="game-board">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+        <div className="game-info">{getStatus()}</div>
+        <div className="restart-button">{renderRestartButton()}</div>
+      </div>
     </div>
   );
 }
